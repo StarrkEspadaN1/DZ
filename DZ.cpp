b9ccd62bcdca4233c821099b7d1c6dd181a0f31d
SigmaYo <romanabbasov026@gmail.com>
2026-02-05 21:12:23 +0500

    Add files via upload

---
+++ DZ.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double p_app = 99, p_or = 111, p_apr = 96, p_pe = 211;
+    double p_tom = 991, p_on = 211, p_cuc = 843;
+    double p_gar = 123, p_par = 99;
+
+    double limit = 2000;
+
+    int app, orng, apr, pear;
+    int tom, on, cuc;
+    int gar, par;
+
+    std::cout << "Введите количество (литры)\n";
+
+    std::cout << "\nФруктовые\n";
+    std::cout << "Яблочный: ";
+    std::cin >> app;
+    std::cout << "Апельсиновый: ";
+    std::cin >> orng;
+    std::cout << "Абрикосовый: ";
+    std::cin >> apr;
+    std::cout << "Грушевый: ";
+    std::cin >> pear;
+
+    std::cout << "\nОвощные\n";
+    std::cout << "Томатный: ";
+    std::cin >> tom;
+    std::cout << "Луковый: ";
+    std::cin >> on;
+    std::cout << "Огуречный: ";
+    std::cin >> cuc;
+
+    std::cout << "\nЧаи\n";
+    std::cout << "Чесночный: ";
+    std::cin >> gar;
+    std::cout << "Петрушевый: ";
+    std::cin >> par;
+
+    double sum = 0;
+    double disc = 0;
+
+    sum += app * p_app;
+    sum += orng * p_or;
+    sum += apr * p_apr;
+    sum += pear * p_pe;
+
+    sum += tom * p_tom;
+    sum += cuc * p_cuc;
+    sum += gar * p_gar;
+
+    double par_sum = par * p_par;
+    if (par >= 3) {
+        double d = par_sum * 0.05;
+        par_sum -= d;
+        disc += d;
+    }
+    sum += par_sum;
+
+    if (sum > limit) {
+        double d = sum * 0.13;
+        sum -= d;
+        disc += d;
+    }
+
+    std::cout << "\n-----------------\n";
+    std::cout << "Скидки: " << disc << " руб.\n";
+    std::cout << "К оплате: " << sum << " руб.\n";
+    std::cout << "-----------------\n";
+}
---
+++ Hmework.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+
+
+const int C = 18;   // вагоны
+const int S = 36;   // места
+
+void fillEmpty(int a[C][S]) {
+    for (int i = 0; i < C; i++)
+        for (int j = 0; j < S; j++)
+            a[i][j] = 0;
+}
+
+void fillRandom(int a[C][S]) {
+    for (int i = 0; i < C; i++)
+        for (int j = 0; j < S; j++)
+            a[i][j] = std::rand() % 2;
+}
+
+void print(int a[C][S]) {
+    for (int i = 0; i < C; i++) {
+        std::cout << "Вагон " << i + 1 << ": ";
+        for (int j = 0; j < S; j++)
+            std::cout << a[i][j] << " ";
+    }
+}
+
+void change(int a[C][S], int c, int s, int v) {
+    a[c][s] = v;
+}
+
+int freeCar(int a[C][S], int c) {
+    int k = 0;
+    for (int j = 0; j < S; j++)
+        if (a[c][j] == 0)
+            k++;
+    return k;
+}
+
+int freeAll(int a[C][S]) {
+    int sum = 0;
+    for (int i = 0; i < C; i++)
+        sum += freeCar(a, i);
+    return sum;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::srand(std::time(0));
+
+    int t[C][S];
+
+    std::cout << "1 - случайно\n2 - всё пусто\n";
+    int m;
+    std::cin >> m;
+
+    if (m == 1)
+        fillRandom(t);
+    else
+        fillEmpty(t);
+
+    print(t);
+
+    int c, s;
+    std::cout << "\n Введите вагон и место: ";
+    std::cin >> c >> s;
+
+    std::cout << "1 - занять, 0 - освободить: ";
+    int v;
+    std::cin >> v;
+
+    change(t, c - 1, s - 1, v);
+
+    std::cout << "\n После изменения: \n";
+    print(t);
+
+    std::cout << "\n Свободных в вагоне " << c << ": "
+        << freeCar(t, c - 1);
+
+    std::cout << "Свободных всего: "
+        << freeAll(t);
+}
